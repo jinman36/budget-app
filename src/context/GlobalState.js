0908d55c8a39e5d346c8362bee20125826515bc8
Jeff Inman <jeffery.inman@hotmail.com>
2021-07-05 16:10:42 -0700

    add submitted information to local storage

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,23 +1,20 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useReducer, useEffect } from 'react'
 import AppReducer from '../context/AppReducer'
 
 const initialState = {
-  incomeTransactions: [
-    { id: 1, incomeText: 'car sold', incomeAmount: 15000 },
-    { id: 2, incomeText: 'salary', incomeAmount: 5300 },
-    { id: 3, incomeText: 'bonus', incomeAmount: 1300 }
-  ],
-  expenseTransactions: [
-    { id: 4, expenseText: 'rent', expenseAmount: 13000 },
-    { id: 5, expenseText: 'food', expenseAmount: 5800 },
-    { id: 6, expenseText: 'liquor', expenseAmount: 100 }
-  ]
+  incomeTransactions: [],
+  expenseTransactions: [],
 };
 
 export const GlobalContext = createContext(initialState);
 
 export const GlobalContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState)
+
+  useEffect(() => {
+    localStorage.setItem('incomeTransactions', JSON.stringify(state.incomeTransactions))
+    localStorage.setItem('expenseTransactions', JSON.stringify(state.expenseTransactions))
+  })
 
   //Income
   const addIncome = incomeTransaction => {
