a887e94d8bc3f5dbd9fc5672256a298e8d08a501
Jeff Inman <jeffery.inman@hotmail.com>
2021-07-05 16:00:49 -0700

    MVP income, expense and total all fuctional but on live state only

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,22 +1,22 @@
-import React, {createContext, useReducer} from 'react'
+import React, { createContext, useReducer } from 'react'
 import AppReducer from '../context/AppReducer'
 
 const initialState = {
   incomeTransactions: [
-    {id: 1, incomeText: 'car sold', incomeAmount: 15000 },
-    {id: 2, incomeText: 'salary', incomeAmount: 5300 },
-    {id: 3, incomeText: 'bonus', incomeAmount: 1300 }
+    { id: 1, incomeText: 'car sold', incomeAmount: 15000 },
+    { id: 2, incomeText: 'salary', incomeAmount: 5300 },
+    { id: 3, incomeText: 'bonus', incomeAmount: 1300 }
   ],
   expenseTransactions: [
-    {id: 4, expenseText: 'rent', expenseAmount: 13000 },
-    {id: 5, expenseText: 'food', expenseAmount: 5800 },
-    {id: 6, expenseText: 'liquor', expenseAmount: 100 }
+    { id: 4, expenseText: 'rent', expenseAmount: 13000 },
+    { id: 5, expenseText: 'food', expenseAmount: 5800 },
+    { id: 6, expenseText: 'liquor', expenseAmount: 100 }
   ]
 };
 
 export const GlobalContext = createContext(initialState);
 
-export const GlobalContextProvider = ({children}) => {
+export const GlobalContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState)
 
   //Income
@@ -26,7 +26,7 @@ export const GlobalContextProvider = ({children}) => {
       payload: incomeTransaction,
     })
   }
-//Expense
+  //Expense
   const addExpense = expenseTransaction => {
     dispatch({
       type: 'ADD_EXPENSE',
@@ -34,12 +34,21 @@ export const GlobalContextProvider = ({children}) => {
     })
   }
 
+  // delete trans
+  const deleteTransaction = (id) => {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id,
+    });
+  };
+
   return (
     <GlobalContext.Provider value={{
-      incomeTransactions: state.incomeTransactions, 
+      incomeTransactions: state.incomeTransactions,
       expenseTransactions: state.expenseTransactions,
       addIncome,
       addExpense,
+      deleteTransaction,
     }}>
       {children}
     </GlobalContext.Provider>
