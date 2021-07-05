319b4ac1342d63a33e7b9a0667538b01ab0c4308
Jeff Inman <jeffery.inman@hotmail.com>
2021-07-04 21:35:53 -0700

    added transactions additona updates to the components

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {v4 as uuidv4} from 'uuid'
 
 const AddTransaction = () => {
+  const [income, setIncome] = useState({
+    incomeText: '',
+    incomeAmount: 0,
+  })
+
+  const {incomeText, incomeAmount} = income
+
+  const onChangeIncome = e => {
+    setIncome({ ...income, [e.target.name]: e.target.value })
+    console.log(income)
+  }
+
+  const onSubmitIncome = e => {
+    e.preventDefault();
+
+    const newIncomeTransaction = {
+      id: uuidv4(),
+      incomeText,
+      incomeAmount: incomeAmount * 1,
+    }
+
+    console.log(newIncomeTransaction)
+  }
+
   return (
     <div className='form-wrapper'>
-      <form>
+      <form onSubmit={onSubmitIncome}>
         <div className='input-group income'>
-          <input type='text' placeholder='Add Income....' autoComplete='off' />
-          <input type='number' placeholder='Amount' autoComplete='off' />
+          <input type='text' name='incomeText' placeholder='Add Income....' autoComplete='off' onChange={onChangeIncome} />
+          <input type='number' name='incomeAmount' placeholder='Amount' autoComplete='off' onChange={onChangeIncome} />
           <input type='submit' value='submit' />
         </div>
       </form>
